14e1a61c11802bc563692d2bde7d5e9c26b06b03
Shuvajit Roy <90036454+S-Roy01@users.noreply.github.com>
2025-03-22 03:08:23 +0600

    Create p_15_formulas.c

---
+++ p_15_formulas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    printf("All Formulas:\n");
+    printf("1. Arithmetic oparetor i.a+b\n ii.a-b\niii.a*b\niv.a/b\nv.a%b\n");
+    printf("2. Leap year: year%400,year%100,year%4\n");
+    printf("3. Swap: c=b,b=a,a=c\n");
+    printf("4. Fahrenheight Celcius: f= (c*(9/5)) + 32, c=(f-32)*5/9, ");
+    printf("5. Area of Triangle: 0.5 * base * height\n");
+    printf("6. Area of Circle: 3.1416* r^2 Circumference:2*3.1416*r\n");
+    printf("7. cm to inch: inch = cm / 2.54 & inch to cm: cm = inch * 2.54 \n");
+    printf("8. Greatest number: if else\n");
+    printf("9. Smallest number: if else\n");
+    printf("10. Prime Number: if else if else \n");
+    printf("11. even odd: number % 2 == 0 ");
+    printf("12. square , square root: double square = number * number, double squareRoot = sqrt(number);");
+    printf("13. Area of Rectangle: length * width\n");
+    printf("14. CGPA to grade: if else");
+    printf("15. All the formula(This one)");
+    return 0;
+}
