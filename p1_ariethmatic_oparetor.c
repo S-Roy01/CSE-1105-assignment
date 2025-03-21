76775d2d2c7e4b172e8922e0bb7e4a1c180b7d2a
Shuvajit Roy <90036454+S-Roy01@users.noreply.github.com>
2025-03-22 01:18:15 +0600

    Update p1_ariethmatic_oparetor.c

--- p1_ariethmatic_oparetor.c
+++ p1_ariethmatic_oparetor.c
@@ -1,2 +1,26 @@
-#include<stdio.h>
-int main
+
+#include <stdio.h>
+int main(){
+   float a,b,sum,sub,mul,div;
+   int c,d, mod;
+   printf("Enter the value of a: ");
+   scanf("%f",&a);
+   printf("Enter the value of b: ");
+   scanf("%f",&b);
+   sum=a+b;
+   printf("The sum of a & b is: %f \n",sum);
+   sub =a-b;
+   printf("The sub of a & b is: %f\n",sub);
+   mul = a*b;
+   printf("The multiplication of a & b is: %f\n",mul);
+   div = a/b;
+   printf("The div of a & b is: %f\n",div);
+   
+   printf("For Modulas,\n Enter the value of c: ");
+   scanf("%d",&c);
+   printf("\nEnter the value of d: ");
+   scanf("%d",&d);
+   mod = c%d;
+   printf("\nThe mod of a & b is: %d",mod);
+    return 0;
+}
