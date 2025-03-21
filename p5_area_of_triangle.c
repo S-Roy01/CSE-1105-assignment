9b13abf6017e1dadd339ae92ee23c8eefaa09d42
Shuvajit Roy <90036454+S-Roy01@users.noreply.github.com>
2025-03-22 02:03:09 +0600

    Update and rename p5_area_of_triangle to p5_area_of_triangle.c

---
+++ p5_area_of_triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+float b,h,area;
+printf("Input the base");
+scanf("%f",&b);
+printf("\n Input the height");
+scanf("%f",&h);
+area=0.5*b*h;
+printf("\n The area of the triangle is: %f",area);
+return 0;
